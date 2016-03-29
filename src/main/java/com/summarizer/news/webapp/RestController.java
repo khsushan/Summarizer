247ae2dd38fddee9606f3deb531e7f1494085a2d
khsushan <khsushan@gmail.com>
2016-03-29 19:31:24 +0530

    add json mapping

--- src/main/java/com/summarizer/news/webapp/RestController.java
+++ src/main/java/com/summarizer/news/webapp/RestController.java
@@ -8,13 +8,15 @@ import com.summarizer.news.sentence.algorithm.LexRank;
 import com.summarizer.news.sentence.algorithm.Vector;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.util.List;
 
-@Path("/summarize")
+@Path("/controller")
 public class RestController {
     @POST
+    @Path("/test")
     @Consumes("text/plain")
     public Response getMessage(String url) {
         StringBuilder output = new StringBuilder();
@@ -36,5 +38,13 @@ public class RestController {
             output.append("\n");
         }
         return Response.status(200).entity(output.toString()).build();
+    }
+
+    @POST
+    @Path("/test2")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response postStudentRecord(JSON_Response json_response){
+        String result = "Record entered: "+ json_response;
+        return Response.status(201).entity(result).build();
     }
 }
