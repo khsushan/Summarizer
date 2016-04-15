dfecc8497436b7ae3f9dc94a5a1d15b05a18c699
khsushan <khsushan@gmail.com>
2016-04-15 20:14:43 +0530

    add fomated api's

--- src/main/java/com/summarizer/news/webapp/RestController.java
+++ src/main/java/com/summarizer/news/webapp/RestController.java
@@ -45,6 +45,25 @@ public class RestController {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response postStudentRecord(JSON_Response json_response){
         String result = "Record entered: "+ json_response;
-        return Response.status(201).entity(result).build();
+        StringBuilder output = new StringBuilder();
+        //readHTML("http://www.dailynews.lk/?q=2016/03/09/local/thai-deputy-prime-minister-meets-foreign-minister");
+        StringBuilder htmlContent = null;
+        try {
+            htmlContent = HtmlReader.readHTML(url);
+            //readHTML("http://www.dailynews.lk/?q=2016/03/09/local/thai-deputy-prime-minister-meets-foreign-minister");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        LexRank lexRank = new LexRank(new StringBuilder[]{htmlContent});
+        double[] lexScore = lexRank.getLexScore();
+
+        Vector.printVector(lexRank.getLexScore());
+        List<String> allSentence = lexRank.getAllSentence();
+        for (int i = 0; i < allSentence.size();i++){
+            output.append(allSentence.get(i)+"======================="+lexScore[i]+"\n");
+            output.append("\n");
+        }
+        return Response.status(200).entity(output.toString()).build();
+
     }
 }
