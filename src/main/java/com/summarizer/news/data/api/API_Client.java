acc47bdc72fec7a9b66fb991d00e0d56e2729c09
khsushan <khsushan@gmail.com>
2016-05-08 13:32:56 +0530

    change news source api to bing

--- src/main/java/com/summarizer/news/data/api/API_Client.java
+++ src/main/java/com/summarizer/news/data/api/API_Client.java
@@ -9,15 +9,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.Base64;
 
 /**
  * Created by ushan on 3/16/16.
  */
 public class API_Client {
 
-    private   static StringBuilder httpClient(String endpoint) throws IOException {
+    private   static StringBuilder httpClient(String endpoint, String authenticationKey) throws IOException {
         URL url = new URL(endpoint);
         URLConnection connection = url.openConnection();
+        if (authenticationKey != null) {
+            String accountKeyEnc =Base64.getEncoder().encodeToString((authenticationKey+ ":"
+                    + authenticationKey).getBytes());
+            connection.setRequestProperty("Authorization","Basic "+accountKeyEnc);
+        }
         String line;
         StringBuilder response = new StringBuilder();
         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -33,11 +41,18 @@ public class API_Client {
      *
      * */
     public static JsonArray getNewsUrls(String keyword) throws IOException {
-        StringBuilder builder = httpClient("https://ajax.googleapis.com/ajax/services/search/news?" +
-                "v=1.0&q="+keyword+"&userip=INSERT-USER-IP");
+//        StringBuilder builder = httpClient("https://ajax.googleapis.com/ajax/services/search/news?" +
+//                "v=1.0&q="+keyword+"&userip=INSERT-USER-IP");
+        String bingUrlPattern = "https://api.datamarket.azure.com/Bing/Search/News?Query=%%27%s%%27&$format=JSON";
+        String query = URLEncoder.encode(keyword, Charset.defaultCharset().name());
+        String url =   String.format(bingUrlPattern, query);
+        StringBuilder builder = httpClient(url,
+                "9KbdApiBCyeSbEJPhFmPf7yD8lnTj/CdDrxFl4G4ZEc");
+
         JsonParser jsonParser =  new JsonParser();
         JsonObject responseObj = (JsonObject) jsonParser.parse(builder.toString());
-        JsonObject responseDataObj = responseObj.get("responseData").getAsJsonObject();
+        System.out.println(responseObj.toString());
+        JsonObject responseDataObj = responseObj.get("d").getAsJsonObject();
         JsonArray results = responseDataObj.get("results").getAsJsonArray();
         return results;
     }
@@ -45,7 +60,7 @@ public class API_Client {
 
     public static void findSynonyms(String word) throws IOException {
         StringBuilder builder =
-                httpClient("http://words.bighugelabs.com/api/2/776aeb1a8c0ca084de2fec4d6d90c441/"+word+"/json");
+                httpClient("http://words.bighugelabs.com/api/2/776aeb1a8c0ca084de2fec4d6d90c441/"+word+"/json",null);
         JsonParser jsonParser =  new JsonParser();
         JsonObject responseObj = (JsonObject) jsonParser.parse(builder.toString());
         JsonObject verbObj = responseObj.get("verb").getAsJsonObject();
