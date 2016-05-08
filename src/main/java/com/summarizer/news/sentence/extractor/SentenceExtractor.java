d434e99c84115c099e27781dd642781a43bca94c
khsushan <khsushan@gmail.com>
2016-05-08 13:32:12 +0530

    add stopword support

--- src/main/java/com/summarizer/news/sentence/extractor/SentenceExtractor.java
+++ src/main/java/com/summarizer/news/sentence/extractor/SentenceExtractor.java
@@ -1,17 +1,12 @@
 package com.summarizer.news.sentence.extractor;
 
-import com.summarizer.news.data.html.HtmlReader;
 import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.ling.Sentence;
 import edu.stanford.nlp.process.DocumentPreprocessor;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-import java.text.BreakIterator;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 /**
  * Created by ushan on 3/5/16.
@@ -21,23 +16,27 @@ public class SentenceExtractor {
     private List<String> allWords =
             new ArrayList<String>(); //this all words set will hold all the unique words in wordsInDocuments
     private List<String> allSentences = new ArrayList<String>();
+    private static ArrayList<String> stopwords;
 
-    public  void getExtractedWordInGivenDocument(StringBuilder builder){
-        Reader reader = new StringReader(builder.toString());
-        DocumentPreprocessor dp = new DocumentPreprocessor(reader);
-        for (List<HasWord> sentence : dp) {
-            String sentenceString = Sentence.listToString(sentence);
-            allSentences.add(sentenceString.toString());
-            String[] tokenizedTerms = sentenceString.toString().
-                    replaceAll("[\\W&&[^\\s]]", "").split("\\W+");//to get individual terms
-            for (String term : tokenizedTerms) {
-                if (!allWords.contains(term)) {  //avoid duplicate entry
-                    allWords.add(term);
+    public  void getExtractedWordInGivenDocument(StringBuilder builder) throws IOException {
+        if(builder != null){
+            Reader reader = new StringReader(builder.toString());
+            DocumentPreprocessor dp = new DocumentPreprocessor(reader);
+            for (List<HasWord> sentence : dp) {
+                String sentenceString = Sentence.listToString(sentence);
+                allSentences.add(sentenceString.toString());
+                String[] tokenizedTerms = sentenceString.toString().
+                        replaceAll("[\\W&&[^\\s]]", "").split("\\W+");//to get individual terms
+                for (String term : tokenizedTerms) {
+                    if (!allWords.contains(term) && !getStopWords().contains(term)) {  //avoid duplicate entry
+                        allWords.add(term);
+                    }
+
                 }
-
+                wordsInDocuments.add(tokenizedTerms);
             }
-            wordsInDocuments.add(tokenizedTerms);
         }
+
     }
 
     public  List<String[]> getWordsInDocuments(){
@@ -51,5 +50,21 @@ public class SentenceExtractor {
     public  List<String> getAllSentences(){
         return  this.allSentences;
     }
+
+    public static ArrayList getStopWords() throws IOException {
+
+        if(stopwords == null){
+            stopwords = new ArrayList<String>();
+            File stopword = new File("src/main/resources/stopword/english/Stopword.txt");
+            BufferedReader stopWordBufferReader =  new BufferedReader(new FileReader(stopword));
+            while (stopWordBufferReader.readLine() != null){
+                stopwords.add(stopWordBufferReader.readLine());
+            }
+            return stopwords;
+        }else {
+            return stopwords;
+        }
+    }
+
 
 }
