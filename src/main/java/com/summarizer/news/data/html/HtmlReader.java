a252a3b3f3ccda755d96c2fe5a5941c10a78d9a2
khsushan <khsushan@gmail.com>
2016-05-08 13:38:53 +0530

    remove unwanted main method

--- src/main/java/com/summarizer/news/data/html/HtmlReader.java
+++ src/main/java/com/summarizer/news/data/html/HtmlReader.java
@@ -1,7 +1,5 @@
 package com.summarizer.news.data.html;
 
-import com.summarizer.news.sentence.algorithm.LexRank;
-import com.summarizer.news.sentence.algorithm.Vector;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -9,7 +7,6 @@ import org.jsoup.select.Elements;
 
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * This class is use to read html content in given link
@@ -17,7 +14,7 @@ import java.util.List;
 public class HtmlReader {
 
     public static StringBuilder readHTML(String link) throws IOException {
-        Document document = Jsoup.connect(link).get();
+        Document document = Jsoup.connect(link).timeout(3000).get();
         String title = document.title();
         Elements paragraphs = document.body().select("p");
         StringBuilder content = new StringBuilder();
@@ -35,24 +32,4 @@ public class HtmlReader {
         }
         return content;
     }
-
-//    public static void main(String[] args) {
-//        try {
-//            //readHTML("http://www.dailynews.lk/?q=2016/03/09/local/thai-deputy-prime-minister-meets-foreign-minister");
-//            StringBuilder htmlContent =
-//                    readHTML("http://www.dailynews.lk/?q=2016/03/09/local/thai-deputy-prime-minister-meets-foreign-minister");
-//            LexRank lexRank = new LexRank(new StringBuilder[]{htmlContent});
-//            double[] lexScore = lexRank.getLexScore();
-//
-//            Vector.printVector(lexRank.getLexScore());
-//            List<String> allSentence = lexRank.getAllSentence();
-//            for (int i = 0; i < allSentence.size();i++){
-//                System.out.println(allSentence.get(i)+"======================="+lexScore[i]);
-//            }
-//            //readHTML("http://www.nytimes.com/2016/03/10/world/middleeast/obama-criticizes-the-free-riders-among-americas-allies.html?_r=0");
-//            //readHTML("http://www.dailynews.lk/?q=2016/03/10/local/basil-rajapaksa-released-bail");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
