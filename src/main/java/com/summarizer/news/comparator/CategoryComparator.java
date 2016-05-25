d122552f7dbfaffd7700756198d0f35de938db6b
khsushan <khsushan@gmail.com>
2016-05-26 00:10:53 +0530

    add category comparator

---
+++ src/main/java/com/summarizer/news/comparator/CategoryComparator.java
@@ -0,0 +1,20 @@
+package com.summarizer.news.comparator;
+
+import com.google.gson.JsonObject;
+
+import java.util.Comparator;
+
+/**
+ * Created by Ushan on 5/25/2016.
+ */
+public class CategoryComparator implements Comparator<JsonObject> {
+
+    public int compare(JsonObject o1, JsonObject o2) {
+        double relevance1 = o1.get("relevance").getAsDouble();
+        double relevance2 = o2.get("relevance").getAsDouble();
+        if(relevance1 > relevance2)
+            return 1;
+        else
+            return -1;
+    }
+}
