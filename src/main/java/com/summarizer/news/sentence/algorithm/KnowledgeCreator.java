c9ae86117089f9a2ae1ae763b9805cb213966f78
khsushan <khsushan@gmail.com>
2016-05-26 00:09:34 +0530

    add uncheck knowledge creation for cricket

--- src/main/java/com/summarizer/news/sentence/algorithm/KnowledgeCreator.java
+++ src/main/java/com/summarizer/news/sentence/algorithm/KnowledgeCreator.java
@@ -1,43 +1,82 @@
 package com.summarizer.news.sentence.algorithm;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.summarizer.news.comparator.CategoryComparator;
+import com.summarizer.news.comparator.ScoreComparator;
+import com.summarizer.news.data.api.API_Client;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Ushan on 4/24/2016.
  */
 public class KnowledgeCreator {
+    private static final  String TEMPLATE_PATH =  "src/main/resources/template";
 
-    public String generateKnowledge(String[] senetcnces){
-        HashMap<String,String[]> categories = extractEntites(senetcnces);
-        HashMap<String,Integer> entitiyCount = new HashMap<String, Integer>();
-        HashMap<String,String> pickedEntities = new HashMap<String, String>();
+    public String generateKnowledge(String[] senetcnces) throws IOException {
+        HashMap<String, ArrayList<JsonObject>> categories = extractEntites(senetcnces);
+        CategoryComparator categoryComparator = new CategoryComparator();
         for (String category: categories.keySet()) {
-            String[] entities  = categories.get(category);
-            for (String entity: entities) {
-                if(entitiyCount.containsKey(entity)){
-                    entitiyCount.put(entity,(entitiyCount.get(entity)+1));
-                }else{
-                    entitiyCount.put(entity,1);
-                }
-            }
-            pickedEntities.put(category,getTheMaxOccaranceEntity(entitiyCount));
-            entitiyCount.clear();
+            ArrayList<JsonObject> entities  = categories.get(category);
+            Collections.sort(entities, categoryComparator);
         }
-        return  assignToTemplate(pickedEntities);
+        return  assignToTemplate(categories);
     }
 
-    private HashMap<String,String[]> extractEntites(String[] senetces){
+    private HashMap<String, ArrayList<JsonObject>> extractEntites(String[] senetces) throws IOException {
+        StringBuilder phrase = new StringBuilder();
+        for (String sentence:senetces) {
+            phrase.append(sentence);
+            phrase.append(".");
+        }
+        JsonArray extractedEntities = API_Client.getExtractedEntities(phrase.toString());
+        HashMap<String,ArrayList<JsonObject>> categories = new HashMap<String, ArrayList<JsonObject>>();
+        for(int i = 0; i < extractedEntities.size() ; i++){
+            JsonObject entity  =  extractedEntities.get(i).getAsJsonObject();
+            String type = entity.get("type").getAsString();
+            if(categories.containsKey(type)){
+                categories.get(type).add(entity);
+            }else {
+                ArrayList<JsonObject> category =  new ArrayList<JsonObject>();
+                category.add(entity);
+                categories.put(type,category);
+            }
 
-        return null;
+        }
+        return categories;
     }
 
-    private String assignToTemplate(HashMap<String,String> pickedEntities){
+    private String assignToTemplate(HashMap<String,ArrayList<JsonObject>> pickedEntities) throws IOException {
+        String  type = pickedEntities.get("sport").get(0).get("text").getAsString();
+        StringBuilder output = new StringBuilder();
+        if(type.equalsIgnoreCase("cricket")){
+            Path path = Paths.get(TEMPLATE_PATH+"/cricket");
+            List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+            String firstLine = lines.get(0).replace("country1",pickedEntities.get("country").get(0).get("text").getAsString());
+            firstLine = firstLine.replace("country2",pickedEntities.get("country").get(1).get("text").getAsString());
+            output.append(firstLine);
+            output.append("\n");
+            String secondLine =  lines.get(1);
+            secondLine = secondLine.replace("country",pickedEntities.get("country").get(0).get("text").getAsString());
+            output.append(secondLine);
+            output.append("\n");
+            String thirdLine =  lines.get(2);
+            thirdLine = thirdLine.replace("person",pickedEntities.get("person").get(0).get("text").getAsString());
+            output.append(thirdLine);
+            output.append("\n");
+        }else if(type.equalsIgnoreCase("football")){
 
-        return null;
+        }
+        return output.toString();
     }
-
-    private String getTheMaxOccaranceEntity(HashMap<String,Integer> entitiyCount){
-        return null;
-    }
-
 }
