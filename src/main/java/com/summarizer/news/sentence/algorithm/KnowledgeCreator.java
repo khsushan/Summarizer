563049e9ec968d00e3c5a193a1d0b483ccb538d1
khsushan <khsushan@gmail.com>
2016-05-29 19:38:33 +0530

    add get senetce score service (didn't test yet)

--- src/main/java/com/summarizer/news/sentence/algorithm/KnowledgeCreator.java
+++ src/main/java/com/summarizer/news/sentence/algorithm/KnowledgeCreator.java
@@ -21,58 +21,79 @@ import java.util.List;
  */
 public class KnowledgeCreator {
     //private static final  String TEMPLATE_PATH =  "src/main/resources/template";
+    private String analyzedType;
+
+    public  KnowledgeCreator(String analyzedType){
+        this.analyzedType = analyzedType;
+    }
 
     public String generateKnowledge(String phrase) throws IOException {
         HashMap<String, ArrayList<JsonObject>> categories = extractEntites(phrase);
         CategoryComparator categoryComparator = new CategoryComparator();
-        for (String category: categories.keySet()) {
-            ArrayList<JsonObject> entities  = categories.get(category);
+        for (String category : categories.keySet()) {
+            ArrayList<JsonObject> entities = categories.get(category);
             Collections.sort(entities, categoryComparator);
         }
-        return  assignToTemplate(categories);
+        return selectTemplate(categories);
     }
 
     private HashMap<String, ArrayList<JsonObject>> extractEntites(String phrase) throws IOException {
         JsonArray extractedEntities = API_Client.getExtractedEntities(phrase);
-        HashMap<String,ArrayList<JsonObject>> categories = new HashMap<String, ArrayList<JsonObject>>();
-        for(int i = 0; i < extractedEntities.size() ; i++){
-            JsonObject entity  =  extractedEntities.get(i).getAsJsonObject();
+        HashMap<String, ArrayList<JsonObject>> categories = new HashMap<String, ArrayList<JsonObject>>();
+        for (int i = 0; i < extractedEntities.size(); i++) {
+            JsonObject entity = extractedEntities.get(i).getAsJsonObject();
             String type = entity.get("type").getAsString();
-            if(categories.containsKey(type)){
+            if (categories.containsKey(type)) {
                 categories.get(type).add(entity);
-            }else {
-                ArrayList<JsonObject> category =  new ArrayList<JsonObject>();
+            } else {
+                ArrayList<JsonObject> category = new ArrayList<JsonObject>();
                 category.add(entity);
-                categories.put(type,category);
+                categories.put(type, category);
             }
 
         }
         return categories;
     }
 
-    private String assignToTemplate(HashMap<String,ArrayList<JsonObject>> pickedEntities) throws IOException {
-        //String  type = pickedEntities.get("Sport").get(0).get("text").getAsString();
+    private String selectTemplate(HashMap<String, ArrayList<JsonObject>> pickedEntities) throws IOException {
+
+        StringBuilder output = null;
+        ClassLoader classLoader = getClass().getClassLoader();
+        if (pickedEntities.get("Sport") != null) {
+            String type = pickedEntities.get("Sport").get(0).get("text").getAsString();
+            if (type.equalsIgnoreCase("T20") || type.equalsIgnoreCase("Cricket")) {
+                output = setValuesToTemplate("Cricket",pickedEntities);
+            } else if (type.equalsIgnoreCase("football")) {
+                output = setValuesToTemplate("Foot Ball",pickedEntities);
+            }
+        }else if(analyzedType.equals("Cricket")){
+            output = setValuesToTemplate("Cricket",pickedEntities);
+        }else  if(analyzedType.equals("Foot Ball")){
+            output = setValuesToTemplate("Foot Ball",pickedEntities);
+        }
+        return output.toString();
+    }
+
+    private StringBuilder setValuesToTemplate(String type,HashMap<String, ArrayList<JsonObject>> pickedEntities) throws IOException {
         StringBuilder output = new StringBuilder();
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("template/cricket.txt").getFile());
-        //if(type.equalsIgnoreCase("T20") || type.equalsIgnoreCase("Cricket") ){
+        if(type.equals("Cricket")) {
+            File file = new File(classLoader.getResource("template/cricket.txt").getFile());
             Path path = Paths.get(file.getAbsolutePath());
             List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-            String firstLine = lines.get(0).replace("country1",pickedEntities.get("Country").get(0).get("text").getAsString());
-            firstLine = firstLine.replace("country2",pickedEntities.get("Country").get(1).get("text").getAsString());
+            String firstLine = lines.get(0).replace("country1", pickedEntities.get("Country").get(0).get("text").getAsString());
+            firstLine = firstLine.replace("country2", pickedEntities.get("Country").get(1).get("text").getAsString());
             output.append(firstLine);
             output.append("\n");
-            String secondLine =  lines.get(1);
-            secondLine = secondLine.replace("country",pickedEntities.get("Country").get(0).get("text").getAsString());
+            String secondLine = lines.get(1);
+            secondLine = secondLine.replace("country", pickedEntities.get("Country").get(0).get("text").getAsString());
             output.append(secondLine);
             output.append("\n");
-            String thirdLine =  lines.get(2);
-            thirdLine = thirdLine.replace("person",pickedEntities.get("Person").get(0).get("text").getAsString());
+            String thirdLine = lines.get(2);
+            thirdLine = thirdLine.replace("person", pickedEntities.get("Person").get(0).get("text").getAsString());
             output.append(thirdLine);
             output.append("\n");
-//        }else if(type.equalsIgnoreCase("football")){
-//            System.out.println("Foot ball");
-//        }
-        return output.toString();
+        }
+        return  output;
     }
 }
